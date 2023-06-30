1e97fb3e5691720ff1d7a6434a40b85cbf278e69
sqj <sqjlt@outlook.com>
2023-06-30 09:59:00 +0800

    青青菜鸟的SpringBoot + Vue项目的版本2的基础构建

---
+++ x-admin3/src/main/java/com/lantu/sys/config/MpConfig.java
@@ -0,0 +1,29 @@
+package com.lantu.sys.config;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @Author: sqj
+ * @Date: 2023-6-14 10:32
+ */
+@Configuration
+public class MpConfig {
+
+    /**
+     * 分宜拦截器的配置
+     * @return
+     */
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        return interceptor;
+    }
+
+}
+
+
